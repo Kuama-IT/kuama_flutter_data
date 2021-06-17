74a348fd33fd1981f6bed3351b35975c12db099c
SimoneBressan <simone@kuama.it>
2021-06-17 14:13:39 +0200

    renamed the features 'launcher' to 'app_pages'

--- lib/launcher.dart
+++
@@ -1,5 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-library kuama_data_launcher;
-
-export 'src/features/launcher/repositories/launcher_repository_impl.dart';
--- lib/src/features/launcher/repositories/launcher_repository_impl.dart
+++
@@ -1,9 +0,0 @@
-import 'package:kuama_dart_domain/launcher.dart';
-import 'package:permission_handler/permission_handler.dart';
-
-class LauncherRepositoryImpl implements LauncherRepository {
-  @override
-  Stream<bool> launchAppSettings() async* {
-    yield await openAppSettings();
-  }
-}
